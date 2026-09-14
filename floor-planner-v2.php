86270241840a89f3a5245b0c1e140a3acedd0fea
davemt74-lang <davemt74@gmail.com>
2026-09-14 13:22:13 -0700

    Load group persistence before canonical planner hydration

--- floor-planner-v2.php
+++ floor-planner-v2.php
@@ -6,12 +6,12 @@ require __DIR__ . '/floor-planner-ops-legacy.php';
 $html = (string)ob_get_clean();
 $needle = '<script src="js/floor-planner-ops.js?v=20260912-canonical1"></script>';
 $replacement = '<script src="js/floor-planner-v2.js?v=20260914-3"></script>'
+    . '<script src="js/floor-planner-v2-groups.js?v=20260914-1"></script>'
     . $needle
     . '<script src="js/floor-planner-v2-runtime.js?v=20260914-3"></script>'
     . '<script src="js/floor-planner-v2-enhancements.js?v=20260914-1"></script>'
     . '<script src="js/floor-planner-v2-fixes.js?v=20260914-1"></script>'
-    . '<script src="js/floor-planner-v2-display.js?v=20260914-1"></script>'
-    . '<script src="js/floor-planner-v2-groups.js?v=20260914-1"></script>';
+    . '<script src="js/floor-planner-v2-display.js?v=20260914-1"></script>';
 if (!str_contains($html, $needle)) {
     http_response_code(500);
     echo 'Floor Planner runtime could not be initialized.';
