5c6390eaa5438632452f702e4982a633311f43c8
davemt74-lang <davemt74@gmail.com>
2026-09-14 13:40:29 -0700

    Load group alignment and Gelato rotation layer

--- floor-planner-v2.php
+++ floor-planner-v2.php
@@ -12,7 +12,8 @@ $replacement = '<script src="js/floor-planner-v2.js?v=20260914-3"></script>'
     . '<script src="js/floor-planner-v2-enhancements.js?v=20260914-1"></script>'
     . '<script src="js/floor-planner-v2-fixes.js?v=20260914-1"></script>'
     . '<script src="js/floor-planner-v2-display.js?v=20260914-1"></script>'
-    . '<script src="js/floor-planner-v2-group-state.js?v=20260914-1"></script>';
+    . '<script src="js/floor-planner-v2-group-state.js?v=20260914-1"></script>'
+    . '<script src="js/floor-planner-v2-group-align.js?v=20260914-1"></script>';
 if (!str_contains($html, $needle)) {
     http_response_code(500);
     echo 'Floor Planner runtime could not be initialized.';
